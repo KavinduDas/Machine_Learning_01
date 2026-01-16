ba71199ffd796376923c7fd03d0a64bc97acba4b
Kavindu Dassanayake <kavindud@AFSL.local>
2026-01-17 02:47:18 +0530

    Add a Logging & Deployment

--- app.py
+++ app.py
@@ -1,26 +1,37 @@
 from flask import Flask ,render_template ,request , redirect
 from helper import preprocessing ,vectorizer ,get_prediction
+from loggers import logging
 
 app = Flask(__name__)
 
+logging.info('Flask server started')
+
 data = dict()
-reviews = ["Good Product" , "Bad Prodcut" , "Awesome"]
-positive = 2
-negative = 1
+reviews = []
+positive = 0
+negative = 0
 
 @app.route("/")
+
+
 
 def index():
     data["reviews"] = reviews
     data["positive"] = positive
     data["negative"] = negative
+
+    logging.info("==Opens Web Page==")
     return render_template("index.html" ,data=data)
 
 @app.route("/", methods = ["POST"])
 
 def my_post():
     text = request.form['text']
+    logging.info(f"Text : {text}")
+
     prepocessed_text = preprocessing(text)
+    logging.info(f"Prepoccessed Text : {prepocessed_text}")
+
     vectorized_text = vectorizer(prepocessed_text)
     prediction = get_prediction(vectorized_text)
 
